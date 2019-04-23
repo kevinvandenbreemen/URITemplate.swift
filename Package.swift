b9b6d64e920fd9f737ccb21ef909620f7bb342f9
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-04-22 22:20:43 -0400

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -1,10 +1,12 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
   name: "URITemplate",
   dependencies: [
-    .Package(url: "https://github.com/kylef/Spectre", majorVersion: 0, minor: 7),
-    .Package(url: "https://github.com/kylef/PathKit", majorVersion: 0, minor: 7),
+    .package(url: "https://github.com/kylef/Spectre", from: "0.9.0"),
+    .package(url: "https://github.com/kylef/PathKit", from: "1.0.0"),
   ],
   swiftLanguageVersions: [3, 4]
 )
